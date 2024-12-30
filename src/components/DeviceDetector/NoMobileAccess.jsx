01e2d3c4b1ec812a29ae039e92fe97d23fff964e
Edu <luis_100404@hotmail.com>
2024-12-30 20:55:02 -0300

    put css in mobile acess

--- src/components/DeviceDetector/NoMobileAccess.jsx
+++ src/components/DeviceDetector/NoMobileAccess.jsx
@@ -1,6 +1,9 @@
+///Style
+import Style from "./NoMobileAccess.module.css";
+
 const NoMobileAccess = () => {
   return (
-    <div>
+    <div className={Style.divPrincNoMobile}>
       <h1>Acesso restrito a dispositivos móveis</h1>
       <p>Por favor, acesse este site de um notebook ou computador.</p>
     </div>
